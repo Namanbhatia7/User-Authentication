689eeb127eaa29faf62cfffcea8098daf97927e1
Naman Bhatia <bhatia.naman@hotmail.com>
2021-03-10 22:00:09 +0530

    mongoose setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
 
 app.get('/', (req,res) =>{
     res.render("Home")
