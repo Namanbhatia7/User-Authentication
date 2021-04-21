9a56e5c333b80289519e4fd6d0fe92ac0ce61fbb
Naman Bhatia <bhatia.naman@hotmail.com>
2021-04-21 15:56:21 +0530

    new serialize code,google callback setup

--- app.js
+++ app.js
@@ -46,8 +46,16 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+  
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
@@ -64,6 +72,16 @@ passport.use(new GoogleStrategy({
 
 app.get('/', (req,res) =>{
     res.render("home")
+})
+
+app.get('/auth/google', (req,res) =>{
+    passport.authenticate('google', { scope: ["profile"] }) 
+})
+
+app.get('/auth/google/secrets', 
+passport.authenticate("google", { failureRedirect: "/login"})
+,(req,res) =>{
+    res.redirect("/secrets"); 
 })
 
 app.get('/login', (req,res) =>{
